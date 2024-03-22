4852e1fe81febfa6f48bbc7577538839bf9f8ee0
branks <jonny@branks.co.uk>
2024-03-22 10:39:53 +0000

    fix typos

---
+++ assets/js/3814ffbc.c7a2db9f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_smrtzr_docs=self.webpackChunk_smrtzr_docs||[]).push([[312],{1966:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- assets/js/5e95c892.22f0b180.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksmartzer=self.webpackChunksmartzer||[]).push([[647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});s(6540);var a=s(4164),t=s(1003),u=s(7559),c=s(2831),n=s(781),l=s(4848);function d(e){return(0,l.jsx)(t.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,l.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
